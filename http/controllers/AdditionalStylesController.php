935263a1e3a17bc5c507c92ca9116e7ed0867822
Samuell <Samuell1@users.noreply.github.com>
2020-01-20 09:55:55 +0100

    Add Cache-Control for Styles route

--- http/controllers/AdditionalStylesController.php
+++ http/controllers/AdditionalStylesController.php
@@ -9,6 +9,7 @@ class AdditionalStylesController extends Controller
     public function render()
     {
         return Response::make(Settings::renderCss(), 200)
-            ->header('Content-Type', 'text/css');
+            ->header('Content-Type', 'text/css')
+            ->header('Cache-Control', 'public, max-age=86400');
     }
 }
