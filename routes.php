ef762e2bd138950330b3ea5fe53818a709bfa673
Samuell <Samuell1@users.noreply.github.com>
2021-04-03 11:33:22 +0200

    Allow only image extension

--- routes.php
+++ routes.php
@@ -1,8 +1,13 @@
 <?php
 
+use Samuell\ContentEditor\Http\Middleware\EditorPermissionsMiddleware;
+
 Route::group(['prefix' => 'contenteditor'], function () {
-    Route::post('image/upload', 'Samuell\ContentEditor\Http\Controllers\ImageController@upload');
-    Route::post('image/save', 'Samuell\ContentEditor\Http\Controllers\ImageController@save');
+
+    Route::middleware(['web', EditorPermissionsMiddleware::class])->group(function () {
+        Route::post('image/upload', 'Samuell\ContentEditor\Http\Controllers\ImageController@upload');
+        Route::post('image/save', 'Samuell\ContentEditor\Http\Controllers\ImageController@save');
+    });
 
     // Additional styles route
     Route::get('styles', 'Samuell\ContentEditor\Http\Controllers\AdditionalStylesController@render');
